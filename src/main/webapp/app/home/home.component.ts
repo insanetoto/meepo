1e514337c534c5020ff30530ef39de624ed8d355
zhangxin <insanetomato@me.com>
2017-02-10 13:29:19 +0800

    login refactor

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { EventManager, JhiLanguageService } from 'ng-jhipster';
-
 import { Account, LoginModalService, Principal } from '../shared';
+
+// 登录使用
+import { LoginService } from '../shared/login/login.service';
+import { StateStorageService } from '../shared/auth/state-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'jhi-home',
@@ -16,14 +20,29 @@ export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
 
+    // 登录参数定义
+    authenticationError: boolean;
+    password: string;
+    rememberMe: boolean;
+    username: string;
+    credentials: any;
+
     constructor(
         private jhiLanguageService: JhiLanguageService,
         private principal: Principal,
         private loginModalService: LoginModalService,
-        private eventManager: EventManager
+        private eventManager: EventManager,
+        // 登录添加
+        private loginService: LoginService,
+        private stateStorageService: StateStorageService,
+        private router: Router,
+
     ) {
         this.jhiLanguageService.setLocations(['home']);
         this.jhiLanguageService.addLocation('login');
+
+        // 登录添加
+        this.credentials = {};
     }
 
     ngOnInit() {
@@ -32,6 +51,7 @@ export class HomeComponent implements OnInit {
         });
         this.registerAuthenticationSuccess();
     }
+
 
     registerAuthenticationSuccess() {
         this.eventManager.subscribe('authenticationSuccess', (message) => {
@@ -47,5 +67,44 @@ export class HomeComponent implements OnInit {
 
     login() {
         this.modalRef = this.loginModalService.open();
+    }
+
+    cancel () {
+        this.credentials = {
+            username: null,
+            password: null,
+            rememberMe: true
+        };
+        this.authenticationError = false;
+    }
+
+    loginMain () {
+        console.log(this.username);
+        this.loginService.login({
+            username: this.username,
+            password: this.password,
+            rememberMe: this.rememberMe,
+        }).then(() => {
+            this.authenticationError = false;
+            if (this.router.url === '/register' || this.router.url === '/activate' ||
+                this.router.url === '/finishReset' || this.router.url === '/requestReset') {
+                this.router.navigate(['']);
+            }
+
+            this.eventManager.broadcast({
+                name: 'authenticationSuccess',
+                content: 'Sending Authentication Success'
+            });
+
+            // // previousState was set in the authExpiredInterceptor before being redirected to login modal.
+            // // since login is succesful, go to stored previousState and clear previousState
+            let previousState = this.stateStorageService.getPreviousState();
+            if (previousState) {
+                this.stateStorageService.resetPreviousState();
+                // this.router.navigate([previousState.name], { queryParams:  previousState.params });
+            }
+        }).catch(() => {
+            this.authenticationError = true;
+        });
     }
 }
